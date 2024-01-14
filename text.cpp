a7b04d141f7545cd23457c87acd4415ac4fe0393
baihua <zhangbohan_wx@sjtu.edu.cn>
2024-01-15 01:25:48 +0800

    Initial commit.

---
+++ text.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zwg_w on 2024/1/15.
+//
+
+#include "text.hpp"
---
+++ text.hpp
@@ -0,0 +1,14 @@
+//
+// Created by zwg_w on 2024/1/15.
+//
+
+#ifndef TEXT_TEXT_HPP
+#define TEXT_TEXT_HPP
+
+
+class text {
+
+};
+
+
+#endif //TEXT_TEXT_HPP
